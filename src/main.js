2821c327bf2bbd24578e92986ea23c39d5772d36
zhaoWJ1994 <www.18756151223@163.com>
2016-07-01 11:20:32 +0800

    zhao

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import store from './vuex/store'
 import "./assets/css/reset.css"
 import "./assets/css/zlayout.css"
 import 'vue-toastr/dist/vue-toastr.css'
+import "./assets/vendor/range/css/ion.rangeSlider.css"
+import "./assets/vendor/range/css/ion.rangeSlider.skinHTML5.css"
+import "./assets/vendor/range/css/normalize.css"
 new Vue({
    el : "body",
    store,
